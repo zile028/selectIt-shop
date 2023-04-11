94188cdd3249949869a299da3ed8b7ba0725836e
crazybarber90 <pepy90aa@gmail.com>
2023-04-11 20:22:18 +0200

    created subscribe component,finished styling and imported on home page

---
+++ frontend/src/component/Subscribe/Subscribe.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Subscribe = () => {
+  return (
+    <div className="subscribe">
+      <div className="top-section">
+        <h5>Our Latest Collection</h5>
+        <h3>SAVE 50% OFF SALE</h3>
+        <p>Be the first known about latest and modern furniture</p>
+      </div>
+      <div className="input">
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Your Email Address"
+        />
+        <span className="input-button">Sign Up</span>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import Subscribe from "../../component/Subscribe/Subscribe";
 
 function HomePage() {
-    return (
-        <div>HOME</div>
-    );
+  return (
+    <>
+      <div>HOME</div>
+      <Subscribe />
+    </>
+  );
 }
 
 export default HomePage;
