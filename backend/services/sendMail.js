7cb178cec04cbbf5a9e778eb7550f845619ecf73
zile028 <zile028@gmail.com>
2023-04-17 19:23:13 +0200

    before merege

--- backend/services/sendMail.js
+++ backend/services/sendMail.js
@@ -1,37 +1,39 @@
 const nodemailer = require("nodemailer");
-const { activationMailTemplate } = require("../mailTemplate/mailTemplate");
+const {activationMailTemplate} = require("../mailTemplate/mailTemplate");
 const {
-  welcomeSubscribeMailTemplate,
+    welcomeSubscribeMailTemplate,
 } = require("../mailTemplate/subMailTemplate");
 const transporter = nodemailer.createTransport({
-  // host: "smtp.gmail.com",
-  // port: 587,
-  // secure: false,
-  service: "gmail",
-  auth: {
-    user: "shopselectit@gmail.com",
-    pass: "qcefsyorjyiwwewm",
-  },
+    // host: "smtp.gmail.com",
+    // port: 587,
+    // secure: false,
+    service: "gmail",
+    auth: {
+        user: "shopselectit@gmail.com",
+        pass: "qcefsyorjyiwwewm",
+    },
 });
 const sendActivateLink = (email, verifyLink) => {
-  let message = {
-    from: "SELECTIT-SHOP <shopselectit@gmail.com>",
-    to: email,
-    subject: "Activate account",
-    html: activationMailTemplate(verifyLink),
-  };
-  return transporter.sendMail(message);
+    let message = {
+        from: "SELECTIT-SHOP <shopselectit@gmail.com>",
+        to: email,
+        subject: "Activate account",
+        html: activationMailTemplate(verifyLink),
+    };
+    return transporter.sendMail(message);
 };
 //This is mail service for successfully subscribed user on SELECTIT-SHOP
 const sendSuccessSubMail = (email, verifyLink) => {
-  let message = {
-    from: "SELECTIT-SHOP <shopselectit@gmail.com>",
-    to: email,
-    subject: "Success Subscription",
-    html: welcomeSubscribeMailTemplate(verifyLink),
-  };
-  return transporter.sendMail(message);
+    let message = {
+        from: "SELECTIT-SHOP <shopselectit@gmail.com>",
+        to: email,
+        subject: "Success Subscription",
+        html: welcomeSubscribeMailTemplate(verifyLink),
+    };
+    return transporter.sendMail(message);
 };
-const resetPassword = () => {};
 
-module.exports = { sendActivateLink, resetPassword, sendSuccessSubMail };
+const resetPassword = () => {
+};
+
+module.exports = {sendActivateLink, resetPassword, sendSuccessSubMail};
