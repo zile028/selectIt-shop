9ff3a6020709575930d64bc7f687a18373b42c29
SalJelena <salopekjelena24@gmail.com>
2023-04-18 18:53:39 +0200

    edit styles on contact page

--- frontend/src/pages/ContactPage/ContactPage.jsx
+++ frontend/src/pages/ContactPage/ContactPage.jsx
@@ -1,11 +1,14 @@
 import ContactForm from "../../component/ContactForm/ContactForm"
 import ContactInfo from "../../component/ContactInfo/ContactInfo"
 import ContactMap from "../../component/ContactMap/ContactMap"
+import Heading from '../../component/Heading/Heading';
+import bgImage from "../../assets/images/contactbanner.jpg"
 
 
 const ContactPage = () => {
   return (
     <>
+        <Heading title="Contact us" bgImage={bgImage} />
         <ContactMap />
         <div className="wrap">
             <div className="contact__container">
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Heading from '../../component/Heading/Heading';
-import bgImage from "../../assets/images/contactbanner.jpg"
 import Subscribe from "../../component/Subscribe/Subscribe";
 
 function HomePage() {
@@ -8,7 +6,6 @@ function HomePage() {
 
     return (
         <>
-            <Heading title="Home page" bgImage={bgImage} />
             <div>HOME</div>
             <Subscribe />
         </>
