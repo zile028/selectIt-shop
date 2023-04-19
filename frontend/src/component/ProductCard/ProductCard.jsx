ffd3500955a6b0a4c67c83b6789ce89a7559c262
SalJelena <salopekjelena24@gmail.com>
2023-04-19 18:30:21 +0200

    edit styles btn

--- frontend/src/component/ProductCard/ProductCard.jsx
+++ frontend/src/component/ProductCard/ProductCard.jsx
@@ -8,6 +8,9 @@ const ProductCard = ({ product }) => {
             <div style={{backgroundImage: "url(" + product.thumbnail + ")"}} className="product__thumbnail"/>
             <div className="product__img-overlay" />
         </div>
+        <div className="product__hover-content">
+            <button className="button button--primary">Add to cart</button>
+        </div>
         <h3 className="product__title">{product.title}</h3>
         <p className="product__price">{product.price}$</p>
     </Link>
