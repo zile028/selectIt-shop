3b25170348250d8a1a92499748cd43ff17414de7
SalJelena <salopekjelena24@gmail.com>
2023-04-19 12:57:42 +0200

    add product card styles in shop comp

---
+++ frontend/src/component/ProductCard/ProductCard.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+const ProductCard = ({ product }) => {
+
+  return (
+    <Link className="product__card">
+        <div className="product__img-holder">
+            <div style={{backgroundImage: "url(" + product.thumbnail + ")"}} className="product__thumbnail"/>
+            <div className="product__img-overlay" />
+        </div>
+        <h3 className="product__title">{product.title}</h3>
+        <p className="product__price">{product.price}$</p>
+    </Link>
+  )
+}
+
+export default ProductCard
--- frontend/src/pages/ShopPage/ShopPage.jsx
+++ frontend/src/pages/ShopPage/ShopPage.jsx
@@ -1,48 +1,71 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSearchParams} from "react-router-dom";
 import ProductService from "../../services/productService";
+import Heading from '../../component/Heading/Heading';
+import bgImage from "../../assets/images/shopbanner.jpg"
+import ProductCard from '../../component/ProductCard/ProductCard';
 
 function ShopPage() {
+    const [products, setProducts] = useState([])
     const [searchParams, setSearchParams] = useSearchParams()
 
     useEffect(() => {
-        let limit = searchParams.get("limit") ? searchParams.get("limit") : 10
+        let limit = searchParams.get("limit") ? searchParams.get("limit") : 9
         let page = searchParams.get("page") ? searchParams.get("page") : 1
         setSearchParams({limit, page})
 
         ProductService.pagination(limit, page)
             .then((res) => {
-                console.log(res.data)
+                setProducts(res.data.products)
             })
             .catch((err) => {
             })
     }, [searchParams]);
 
 
-    return (
-        <section className="container">
-            <div>ShopPage</div>
+    
+    const renderedProducts = () => {
+        return products.map(product => {
+            return <ProductCard product={product} key={product._id} />
+        })
+    }
 
-            <nav aria-label="Page navigation example">
-                <ul className="pagination">
-                    <li className="page-item">
-                        <a className="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
-                    <li className="page-item"><a className="page-link" href="#">1</a></li>
-                    <li className="page-item"><a className="page-link" href="#">2</a></li>
-                    <li className="page-item"><a className="page-link" href="#">3</a></li>
-                    <li className="page-item">
-                        <button className="page-link" aria-label="Next" onClick={() => {
-                            setSearchParams({limit: 10, page: 5})
-                        }}>
-                            <span aria-hidden="true">&raquo;</span>
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </section>
+    return (
+        <>
+            <Heading title="OUR PRODUCTS" bgImage={bgImage}></Heading>
+            <section className="container">
+
+                <div className="products__container">
+                    {renderedProducts()}
+                </div>
+
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item">
+                            <a className="page-link" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li className="page-item">
+                            <a className="page-link">1</a>
+                        </li>
+                        <li className="page-item">
+                            <a className="page-link">2</a>
+                        </li>
+                        <li className="page-item">
+                            <a className="page-link">3</a>
+                        </li>
+                        <li className="page-item">
+                            <button className="page-link" aria-label="Next" onClick={() => {
+                                setSearchParams({limit: 9, page: 5})
+                            }}>
+                                <span aria-hidden="true">&raquo;</span>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        </>
     );
 }
 
