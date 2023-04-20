109db2649787e154df06c18065146cb09a5d659f
crazybarber90 <pepy90aa@gmail.com>
2023-04-21 01:50:15 +0200

    created futter section

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Heading from '../../component/Heading/Heading';
-import bgImage from "../../assets/images/contactbanner.jpg"
+import React from "react";
+import Heading from "../../component/Heading/Heading";
+import bgImage from "../../assets/images/contactbanner.jpg";
 import Subscribe from "../../component/Subscribe/Subscribe";
+import Footer from "../../component/Footer/Footer";
 
 function HomePage() {
-
-
-    return (
-        <>
-            <Heading title="Home page" bgImage={bgImage} />
-            <div>HOME</div>
-            <Subscribe />
-        </>
-    );
+  return (
+    <>
+      <Heading title="Home page" bgImage={bgImage} />
+      <div>HOME</div>
+      <Subscribe />
+      <Footer />
+    </>
+  );
 }
 
 export default HomePage;
