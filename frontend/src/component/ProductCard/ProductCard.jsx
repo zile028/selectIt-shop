a526bafb1f2a40068caee525c0079f69c949bf0d
SalJelena <salopekjelena24@gmail.com>
2023-04-20 15:47:51 +0200

    edit route link in product shop

--- frontend/src/component/ProductCard/ProductCard.jsx
+++ frontend/src/component/ProductCard/ProductCard.jsx
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom"
 const ProductCard = ({ product }) => {
 
   return (
-    <Link className="product__card">
-        <div className="product__img-holder">
-            <div style={{backgroundImage: "url(" + product.thumbnail + ")"}} className="product__thumbnail"/>
-            <div className="product__img-overlay" />
-        </div>
+    <div className="product__card">
+        <Link to={"/product/" + product._id} className="product__img-holder">
+              <div style={{backgroundImage: "url(" + product.thumbnail + ")"}} className="product__thumbnail"/>
+              <div className="product__img-overlay" />
+        </Link>
         <div className="product__hover-content">
             <button className="button button--primary">Add to cart</button>
         </div>
-        <h3 className="product__title">{product.title}</h3>
-        <p className="product__price">{product.price}$</p>
-    </Link>
+        <Link to={"/product/" + product._id} className="product__link">
+          <h3 className="product__title">{product.title}</h3>
+          <p className="product__price">{product.price}$</p>
+        </Link>
+    </div>
   )
 }
 
