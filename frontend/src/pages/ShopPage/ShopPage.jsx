939ae6ddb8e680ffdedf3dc0679e09ae825f2dcd
SalJelena <salopekjelena24@gmail.com>
2023-04-25 19:37:29 +0200

    edit on product view per page

--- frontend/src/pages/ShopPage/ShopPage.jsx
+++ frontend/src/pages/ShopPage/ShopPage.jsx
@@ -13,16 +13,51 @@ import Pagination from '../../component/Pagination/Pagination';
 function ShopPage() {
     const [products, setProducts] = useState([])
     const [count, setCount] = useState()
+    const [perPageView, setPerPageView] = useState([3, 6, 12]);
+    const [selectedView, setSelectedView] = useState(0);
+
     const [searchParams, setSearchParams] = useSearchParams()
     const {category} = useParams()
     const dispatch = useDispatch()
-    let limit = searchParams.get("limit") ? parseInt(searchParams.get("limit")) : 9
+
+    const handlePerPageView = (index) => {
+        setSelectedView(index)
+    }
+
+    const renderedPerPageView = () => {
+        return perPageView.map((view, index) => {
+            return <li key={index}>
+                    <button onClick={() => handlePerPageView(index)}>{view}</button>
+                </li>
+        })
+    }
+
+    let limit = searchParams.get("limit") ? parseInt(searchParams.get("limit")) : perPageView[selectedView]
     let page = searchParams.get("page") ? parseInt(searchParams.get("page")) : 1
 
+    // console.log(limit);
+    // console.log(perPageView[selectedView]);
+    console.log(searchParams);
+
+  
     useEffect(() => {
         let req = null
-        setSearchParams({limit, page})
+        if(perPageView[selectedView] !== limit) {
+            setSearchParams({
+                limit: (searchParams) => {
+                    searchParams.set("limit", perPageView[selectedView].toString())
+                    return searchParams
+                },
+                page
+            })
+        } else {
+            setSearchParams({limit, page})
+        }
+        
         dispatch(setVisibleLoader(true))
+
+        //console.log(limit);
+
         if (category) {
             req = ProductService.categoryPagination(limit, page, category)
         } else {
@@ -35,6 +70,8 @@ function ShopPage() {
             .catch((err) => {
             })
             .finally(() => dispatch(setVisibleLoader(false)))
+
+
     }, [searchParams]);
 
 
@@ -44,7 +81,6 @@ function ShopPage() {
         })
     }
 
-
     return (
         <>
             <Heading title="OUR PRODUCTS" bgImage={bgImage}></Heading>
@@ -52,10 +88,21 @@ function ShopPage() {
                 <Sidebar/>
                 <div className='shop__wrapper'>
                     <Loader/>
+
+                    <div className='shop__view-controls'>
+                        <span>View per page: </span>
+                        <button className='shop__control'>
+                            {perPageView[selectedView]}
+                        </button>
+                        <ul>
+                            {renderedPerPageView()}
+                        </ul>
+                    </div>
+
                     <div className="products__container">
                         {renderedProducts()}
                     </div>
-                    <Pagination setSearchParams={setSearchParams} limit={limit} page={page} count={count} />
+                    {/* <Pagination setSearchParams={setSearchParams} limit={limit} page={page} count={count} /> */}
                 </div>
             </section>
         </>
