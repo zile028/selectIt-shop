da49c1838528429d43ff33385889c77ac2c9722d
SalJelena <salopekjelena24@gmail.com>
2023-04-25 13:24:42 +0200

    set active category styles

--- frontend/src/component/SIdebar/Sidebar.jsx
+++ frontend/src/component/SIdebar/Sidebar.jsx
@@ -1,44 +1,61 @@
-import React, {useEffect, useState} from 'react'
-import CategoryServices from '../../services/CategoryServices'
-import {Link} from "react-router-dom";
-import {routes} from "../../router/routes";
+import React, { useEffect, useState } from "react";
+import CategoryServices from "../../services/CategoryServices";
+import { Link } from "react-router-dom";
+import { routes } from "../../router/routes";
+import { MdSearch } from "react-icons/md";
 
 function Sidebar() {
-    const [category, setCategory] = useState([])
+  const [category, setCategory] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null)
 
-    useEffect(() => {
-        CategoryServices.getAllCategory()
-            .then(res => setCategory(res.data))
-            .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    CategoryServices.getAllCategory()
+      .then((res) => setCategory(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    const renderedCategories = () => {
-        return category.map(el => {
-            return (
-                <li key={el._id} className='sidebar__item'>
-                    <Link 
-                        to={routes.CATEGORY_PRODUCTS.realPath(el.name)}
-                        className='sidebar__link'
-                    >
-                        {el.name}
-                    </Link>
-                </li>
-            )
-        })
-    }
+  const renderSelectedCategory = (index) => {
+    setSelectedCategory(index)
+  }
 
-    return (
-        <div className='sidebar'>
-            <form className='sidebar__form'>
-                <label className='sr-only'>Search</label>
-                <input type='text' className='sidebar__input' />
-                <input type='submit' className='sidebar__submit' />
-            </form>
-            <ul>
-                {renderedCategories()}
-            </ul>
-        </div>
-    )
+  const renderedCategories = () => {
+    return category.map((el, index) => {
+      return (
+        <li key={el._id} className="sidebar__list-item">
+          <Link
+            to={routes.CATEGORY_PRODUCTS.realPath(el.name)}
+            className={selectedCategory === index ? `sidebar__list-link--active` : `sidebar__list-link`}
+            onClick={() => renderSelectedCategory(index)}
+          >
+            {el.name}
+          </Link>
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div className="sidebar">
+      <form className="sidebar__form">
+        <label className="sidebar__label" htmlFor="search">
+          Search
+        </label>
+        <input
+          type="text"
+          className="sidebar__input"
+          placeholder="Search..."
+          name="search"
+        />
+        <button type="submit" className="sidebar__submit">
+          <MdSearch />
+        </button>
+      </form>
+      <h3 className="sidebar__title">Categories</h3>
+      <ul className="sidebar__list">
+        {renderedCategories()}
+      </ul>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
