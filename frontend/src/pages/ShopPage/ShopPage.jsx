c5c2dfb8ae78810878e4c824ef261cac1e89bc7e
SalJelena <salopekjelena24@gmail.com>
2023-04-25 12:47:08 +0200

    edit sidebar, pagination and shop styles

--- frontend/src/pages/ShopPage/ShopPage.jsx
+++ frontend/src/pages/ShopPage/ShopPage.jsx
@@ -8,6 +8,7 @@ import bgImage from "../../assets/images/shopbanner.jpg"
 import ProductCard from '../../component/ProductCard/ProductCard';
 import Sidebar from '../../component/SIdebar/Sidebar';
 import Loader from '../../component/Loader/Loader';
+import Pagination from '../../component/Pagination/Pagination';
 
 function ShopPage() {
     const [products, setProducts] = useState([])
@@ -43,62 +44,18 @@ function ShopPage() {
         })
     }
 
-    const handlePreviousPage = () => {
-        if (page > 1) {
-            setSearchParams({limit, page: page - 1})
-        }
-    }
-
-    const handleNextPage = () => {
-        if (page < Math.ceil(count / limit)) {
-            setSearchParams({limit, page: page + 1})
-        }
-    }
-
-    const renderPageBtn = () => {
-        let numberPage = Math.ceil(count / limit)
-
-        return Array(numberPage).fill(1).map((el, index) => {
-            return <li key={index} className="page-item">
-                <button className="page-link" name={el + index} onClick={changePage}>{el + index}</button>
-            </li>
-
-        })
-    }
-
-    const changePage = (e) => {
-        setSearchParams({limit, page: e.target.name})
-    }
 
     return (
         <>
             <Heading title="OUR PRODUCTS" bgImage={bgImage}></Heading>
-
-
             <section className="container products_content">
-
                 <Sidebar/>
                 <div className='shop__wrapper'>
                     <Loader/>
                     <div className="products__container">
                         {renderedProducts()}
                     </div>
-
-                    <nav aria-label="Page navigation example">
-                        <ul className="pagination">
-                            <li className="page-item">
-                                <button className="page-link" aria-label="Previous" onClick={handlePreviousPage}>
-                                    <span aria-hidden="true">&laquo;</span>
-                                </button>
-                            </li>
-                            {count && renderPageBtn()}
-                            <li className="page-item">
-                                <button className="page-link" aria-label="Next" onClick={handleNextPage}>
-                                    <span aria-hidden="true">&raquo;</span>
-                                </button>
-                            </li>
-                        </ul>
-                    </nav>
+                    <Pagination setSearchParams={setSearchParams} limit={limit} page={page} count={count} />
                 </div>
             </section>
         </>
