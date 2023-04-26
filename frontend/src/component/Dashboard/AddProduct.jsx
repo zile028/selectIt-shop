a09635c0b5990230c3ca27a354961230c871641c
zile028 <zile028@gmail.com>
2023-04-26 18:36:55 +0200

    prepare for add product

--- frontend/src/component/Dashboard/AddProduct.jsx
+++ frontend/src/component/Dashboard/AddProduct.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import {useFormik} from "formik";
-import * as Yup from "yup"
 
 function AddProduct() {
-	const formik = useFormik({
-		initialValues: {thumbnail: undefined},
-		validationSchema: Yup.object().shape({
-			thumbnail: Yup.mixed().required("A file is required")
-			  .test("fileSize", "File too large",
-				(value) => {
-					console.log("OVO", value)
-					return value
-				})
-		}),
-		onSubmit: (values) => {
-			console.log(values.thumbnail.size)
-			console.log("test")
-		}
-	})
 
-	return (
-	  <section className="container">
-		  <form onSubmit={formik.handleSubmit}>
-			  <div className="form">
-				  <label htmlFor="">{formik.errors.file && formik.errors.file}</label>
-				  <input type="file" name="thumbnail" onInput={formik.handleChange}/>
-			  </div>
-			  <button className="btn btn-primary" type="submit">Add</button>
-		  </form>
-	  </section>
-	);
+
+    return <></>
+
 }
 
 export default AddProduct;
