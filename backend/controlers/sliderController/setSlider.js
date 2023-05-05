da09ff0901e5c31db3e23e342d5f8a3ce6a0c684
zile028 <zile028@gmail.com>
2023-05-05 18:52:19 +0200

    fixed setSlider

--- backend/controlers/sliderController/setSlider.js
+++ backend/controlers/sliderController/setSlider.js
@@ -1,17 +1,18 @@
 const SliderModel = require("../../model/SliderModel");
+
 const setSlider = (req, res) => {
-  SliderModel.deleteMany({})
-    .then(() => {
-      SliderModel.insertMany(req.body)
-        .then((res) => {
-          res.status(201).send("Slider successfully updated");
+    SliderModel.deleteMany({})
+        .then(() => {
+            SliderModel.insertMany(req.body)
+                .then((result) => {
+                    res.status(201).send("Slider successfully updated");
+                })
+                .catch((err) => {
+                    res.status(417).send(err);
+                });
         })
         .catch((err) => {
-          res.status(417).send(err);
+            res.status(417).send(err);
         });
-    })
-    .catch((err) => {
-      res.status(417).send(err);
-    });
 };
 module.exports = setSlider;
