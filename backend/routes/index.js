151efdcbc40ed66282f4876a39329ef2e11f7d80
zile028 <zile028@gmail.com>
2023-05-05 18:55:27 +0200

    fixed setSlider

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,8 +6,8 @@ router.use("/subscriber", require("./subscriberRoutes"));
 router.use("/contact", require("./contactRoutes"))
 router.use("/product", require("./productRoutes"))
 router.use('/category', require('./categoryRoutes'))
-router.use("/slider", require("./sliderRoutes"));
-router.use('/brand',require('./brandRoutes'));
+// router.use("/slider", require("./sliderRoutes"));
+router.use('/brand', require('./brandRoutes'));
 router.use("/results", require("./searchRoutes"));
 router.use("/cart", require("./cart"));
 router.use("/random", require("./randomProductsRoutes"));
