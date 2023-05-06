2c6f178e2458fbf3e1098af35faadcff7991471c
zile028 <zile028@gmail.com>
2023-05-06 19:31:06 +0200

    fix activate account

--- backend/config/configVars.js
+++ backend/config/configVars.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
-const {JWT_KEY} = process.env
 
 module.exports = {
-    privateKey: JWT_KEY,
+    privateKey: slit123,
     FE_URL: process.env.NODE_ENV === "production" ? "https://select-it-shop-fe.vercel.app/" : "http://localhost:3000/"
 }
--- frontend/src/services/UserServices.js
+++ frontend/src/services/UserServices.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 class UserServices {
-  static register = (body) => axios.post("/user/register", body);
-  static getUser = (body) => axios.post("/user", body);
+    static register = (body) => axios.post("/user/register", body);
+    static getUser = (body) => axios.post("/user", body);
 
-  static login = (body) => axios.post("/user/login", body);
-
-  static addToFavorite = (body) => axios.put("/user/addToFavorite", body);
-  static removeFromFavorite = (body) =>
-    axios.put("/user/removeFromFavorite", body);
+    static login = (body) => axios.post("/user/login", body);
+    static activateAccount = (id) => axios.put("/user/activate/" + id)
+    static addToFavorite = (body) => axios.put("/user/addToFavorite", body);
+    static removeFromFavorite = (body) =>
+        axios.put("/user/removeFromFavorite", body);
 }
 
 export default UserServices;
