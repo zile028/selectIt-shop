47ccd0bd22292dd593c353cf6658592621c03212
zile028 <zile028@gmail.com>
2023-05-06 19:42:17 +0200

    fix activate account

--- backend/config/configVars.js
+++ backend/config/configVars.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 
 module.exports = {
-    privateKey: slit123,
+    privateKey: "slit123",
     FE_URL: process.env.NODE_ENV === "production" ? "https://select-it-shop-fe.vercel.app/" : "http://localhost:3000/"
 }
